82f02c8dd8cea0ed1411f07501dee60f5a8eeb6e
Ryan <ryfly2016@gmail.com>
2025-04-09 10:32:06 -0600

    create store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import api from "../api/api";
 
 // TODO: configure the store to use the API slice's auto-generated reducer and custom middleware.
-const store = configureStore();
+const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+});
+
+setupListeners(store.dispatch);
 
 export default store;
