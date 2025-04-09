e5127d6193a6e3df6b24705ada0702cde2449cc3
Ryan <ryfly2016@gmail.com>
2025-04-09 13:07:20 -0600

    put index.html in public, switch from vite-react-swc to vite-react so it works, change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        app: "./public/index.html",
+      },
+    },
+  },
+  server: {
+    open: "./public/index.html",
+  },
 });
