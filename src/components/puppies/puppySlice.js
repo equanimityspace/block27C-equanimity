0ea6005c649e7f43bfa3443279e8cadfcfb246ff
Ryan <ryfly2016@gmail.com>
2025-04-09 10:32:23 -0600

    create puppySlice

--- src/components/puppies/puppySlice.js
+++ src/components/puppies/puppySlice.js
@@ -1,11 +1,12 @@
+import { buildErrorMessage } from "vite";
 import api from "../../api/api";
 
 /*
 TODO: Define the following 4 endpoints:
-  1. getPuppies (query)
-  2. getPuppy (query)
-  3. addPuppy (mutation)
-  4. deletePuppy (mutation)
+  1. getPuppies (query) - Done
+  2. getPuppy (query) - Done
+  3. addPuppy (mutation) - Done
+  4. deletePuppy (mutation) - Done
 
 The query endpoints should provide the "Puppy" tag.
 The mutation endpoints should invalidate the "Puppy" tag.
@@ -15,7 +16,55 @@ functions for each endpoint.
 */
 
 const puppyApi = api.injectEndpoints({
-  endpoints: (build) => ({}),
+  endpoints: (build) => ({
+    getPuppies: build.query({
+      query: ({ id, name, imageURL }) => ({
+        url: "/players",
+        method: "GET",
+        body: {
+          id,
+          name,
+          imageURL,
+        },
+      }),
+      providesTags: ["Puppy"],
+    }),
+    getPuppy: build.query({
+      query: ({ id, breed, status }) => ({
+        url: "/players",
+        method: "GET",
+        body: {
+          id,
+          breed,
+          status,
+        },
+      }),
+      providesTags: ["Puppy"],
+    }),
+    addPuppy: build.mutation({
+      mutation: ({ name, breed, status, imageURL }) => ({
+        url: "/players",
+        method: "POST",
+        body: {
+          name,
+          breed,
+          status,
+          imageURL,
+        },
+      }),
+      invalidatesTags: ["Puppy"],
+    }),
+    deletePuppy: build.mutation({
+      mutation: ({ id }) => ({
+        url: "/players",
+        method: "DELETE",
+        body: {
+          id,
+        },
+      }),
+      invalidatesTags: ["Puppy"],
+    }),
+  }),
 });
 
 export const {
